c794df86ea55c15fb33aba85f33c9631c1a6be39
Hossam <hossam.ghareb@inovaeg.com>
2014-03-16 16:43:25 +0200

    Create The Custom Alert View

---
+++ HGAlertView.h
@@ -0,0 +1,22 @@
+//
+//  HGAlertView.h
+//  Hossam Ghareeb
+//
+//  Created by Hossam on 3/16/14.
+//  Copyright (c) 2014 Hossam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+typedef void (^HGAlerViewCompletionHandler)(id alertView, int buttonIndex);
+
+@interface HGAlertView : UIAlertView <UIAlertViewDelegate>
+
+
+
+-(id)initWithTitle:(NSString *)title message:(NSString *)message delegate:(id)delegate cancelButtonTitle:(NSString *)cancelButtonTitle otherButtonTitles:(NSString *)otherButtonTitles, ...;
+
+-(void)showWithCompltionHandler:(HGAlerViewCompletionHandler)handler;
+
+@end
---
+++ HGAlertView.m
@@ -0,0 +1,59 @@
+//
+//  HGAlertView.m
+//  Hossam Ghareeb
+//
+//  Created by Hossam on 3/16/14.
+//  Copyright (c) 2014 Hossam. All rights reserved.
+//
+
+#import "HGAlertView.h"
+
+@interface HGAlertView ()
+
+@property (nonatomic, weak) HGAlerViewCompletionHandler handler;
+
+
+@end
+@implementation HGAlertView
+
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+-(id)init
+{
+    self  = [super init];
+    if (self) {
+        self.delegate = self;
+    }
+    
+    return self;
+}
+
+-(id)initWithTitle:(NSString *)title message:(NSString *)message delegate:(id)delegate cancelButtonTitle:(NSString *)cancelButtonTitle otherButtonTitles:(NSString *)otherButtonTitles, ...
+{
+    self = [super initWithTitle:title message:message delegate:delegate cancelButtonTitle:cancelButtonTitle otherButtonTitles:otherButtonTitles, nil];
+    if (self) {
+        self.delegate = self;
+    }
+    return self;
+}
+
+-(void)showWithCompltionHandler:(HGAlerViewCompletionHandler)handler
+{
+    self.handler = handler;
+    [super show];
+}
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    
+    self.handler(self, buttonIndex);
+}
+
+@end
